4272427764f667ba4cd45abcbee9fcfb7303fb38
Aradhya Nepal <aradhya.1221@gmail.com>
2024-04-21 08:18:50 +0545

    Working with youtube

--- lib/features/home/home_screen.dart
+++ lib/features/home/home_screen.dart
@@ -1,15 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:playlist_simplified_app/features/home/widget/youtube_screen.dart';
 
 class HomeScreen extends StatelessWidget {
-  static const String route="/home-screen";
+  static const String route = "/home-screen";
+
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return PageView(
-      children: [
-
-      ],
+    final items = [
+      const YoutubeScreen(videoId: "CUT_hdgbV1I"),
+      const YoutubeScreen(videoId: "v=VAnAIi-Vdcg"),
+      const YoutubeScreen(videoId: "yRyiswHx07I"),
+      const YoutubeScreen(videoId: "m2OpffdcjNM"),
+    ];
+    return PageView.builder(
+      itemBuilder: (context, index) {
+        return items[index];
+      },
+      itemCount: items.length,
     );
   }
 }
--- lib/features/home/widget/youtube_screen.dart
+++ lib/features/home/widget/youtube_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:playlist_simplified_app/common/constants/color_constants.dart';
 import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
@@ -22,57 +21,46 @@ class _YoutubeScreenState extends State<YoutubeScreen> {
     flags: const YoutubePlayerFlags(
       mute: false,
       loop: true,
-      autoPlay: true
+      autoPlay: false,
     ),
   );
 
+  @override
+  void initState() {
+    super.initState();
+    _controller.addListener(() {
+
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        YoutubePlayer(
-          onEnded: (_){
-            Navigator.pop(context);
-          },
-          controller: _controller,
-          bottomActions:[
-            const SizedBox(width: 14.0),
-            CurrentPosition(),
-            const SizedBox(width: 8.0),
-            ProgressBar(
-              isExpanded: true,
-              colors: ProgressBarColors(
-                playedColor: ColorConstants.primaryColor,
-                bufferedColor: Colors.white,
-                backgroundColor: ColorConstants.primaryColor.withOpacity(0.5),
-                handleColor: ColorConstants.primaryColor,
-              ),
-            ),
-            RemainingDuration(),
-            const PlaybackSpeedButton(),
-          ],
-        ),
-        Positioned(
-          top: MediaQuery.of(context).viewPadding.top+20.h,
-          left: 10.w,
-          child: Container(
-            decoration: BoxDecoration(
-              color: Colors.black.withOpacity(0.85),
-              shape: BoxShape.circle,
-            ),
-            child: IconButton(
-              style: iconStyleFrom,
-              onPressed: (){
-                Navigator.pop(context);
-              },
-              icon: const Icon(
-                Icons.arrow_back,
-                color: Colors.white,
-              ),
-            ),
+    return YoutubePlayer(
+      onEnded: (_){
+        Navigator.pop(context);
+      },
+      controller: _controller,
+      bottomActions:[
+        const SizedBox(width: 14.0),
+        CurrentPosition(),
+        const SizedBox(width: 8.0),
+        ProgressBar(
+          isExpanded: true,
+          colors: ProgressBarColors(
+            playedColor: ColorConstants.primaryColor,
+            bufferedColor: Colors.white,
+            backgroundColor: ColorConstants.primaryColor.withOpacity(0.5),
+            handleColor: ColorConstants.primaryColor,
           ),
         ),
+        RemainingDuration(),
+        const PlaybackSpeedButton(),
       ],
     );
   }
